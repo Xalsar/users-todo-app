9180af08a8a506ea67c3ae0983188709f6220f93
Xalsar <xavicarreragimbert@gmail.com>
2020-11-26 22:08:23 +0100

    Add user start_time and end_time to Todo model

--- backend/todo/models.py
+++ backend/todo/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 
+
 class User(models.Model):
     name = models.CharField(max_length=100)
     email = models.CharField(max_length=100)
@@ -11,13 +12,15 @@ class User(models.Model):
 
 
 class Todo(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, related_name="todos",
+                             on_delete=models.CASCADE)
     title = models.CharField(max_length=120)
     description = models.TextField()
     completed = models.BooleanField(default=False)
-    start_time = models.DateTimeField("Starting time for the task", default=timezone.now, blank=True)
-    end_time = models.DateTimeField("Time when task was ended", blank=True, null=True)
+    start_time = models.DateTimeField(
+        "Starting time for the task", default=timezone.now, blank=True)
+    end_time = models.DateTimeField(
+        "Time when task was ended", blank=True, null=True)
 
     def _str_(self):
         return self.title
-
