fb5d27ff49670c60e4d2d92ee66f441462f0afa1
Xalsar <xavicarreragimbert@gmail.com>
2020-11-26 22:10:01 +0100

    Add User and Todo views

--- backend/todo/views.py
+++ backend/todo/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import User, Todo
+from .serializers import UserSerializer, TodoSerializer
 
-# Create your views here.
+
+class UserView(viewsets.ModelViewSet):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+
+class TodoView(viewsets.ModelViewSet):
+    serializer_class = TodoSerializer
+    queryset = Todo.objects.all()
