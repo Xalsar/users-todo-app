9588e1c47187b72fc92728c65d627472b9ca56e7
Xalsar <xavicarreragimbert@gmail.com>
2020-11-27 22:35:05 +0100

    Add view to save todo

--- backend/todo/views.py
+++ backend/todo/views.py
@@ -3,16 +3,38 @@ from rest_framework import viewsets
 from rest_framework.decorators import api_view
 from django.http.response import JsonResponse
 from rest_framework.parsers import JSONParser
+from rest_framework import status
 from .models import User, Todo
 from .serializers import UserSerializer, TodoSerializer
 
 # Users
 
 
+@api_view(['GET', 'POST'])
 def list_users(request):
     if request.method == 'GET':
         users = User.objects.all()
         usersSerialized = UserSerializer(users, many=True)
         return JsonResponse(usersSerialized.data, safe=False)
     if request.method == 'POST':
-        return JsonResponse("Hello!", safe=False)
+        todoData = JSONParser().parse(request)
+
+        try:
+            todoData["title"]
+            todoData["description"]
+            todoData["userId"]
+        except:
+            return JsonResponse({'message': 'User does not exist'}, status=status.HTTP_400_BAD_REQUEST)
+
+        try:
+            user = User.objects.get(pk=todoData['userId'])
+        except User.DoesNotExist:
+            return JsonResponse({'message': 'User does not exist'}, status=status.HTTP_404_NOT_FOUND)
+
+        user.todos.create(
+            title=todoData["title"], description=todoData["description"]
+        )
+
+        user.save()
+
+        return JsonResponse(UserSerializer(user), safe=False)
