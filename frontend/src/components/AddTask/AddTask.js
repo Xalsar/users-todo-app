24a3a17dd30630b06b0e43c534ebc39e6e3dd40b
Xalsar <xavicarreragimbert@gmail.com>
2020-11-27 23:14:18 +0100

    Add validation and loading spinner AddTask

--- frontend/src/components/AddTask/AddTask.js
+++ frontend/src/components/AddTask/AddTask.js
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
 import axios from "axios";
-import { Modal, Button, Form } from "react-bootstrap";
+import { Modal, Button, Form, Spinner } from "react-bootstrap";
 
-const AddTask = ({ show, onClose, userId }) => {
-  console.log(userId);
+const AddTask = ({ show, onClose, userId, onSucceed }) => {
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
+  const [validated, setValidated] = useState(false);
+  const [loading, setLoading] = useState(false);
 
   const saveTask = () => {
     axios
@@ -14,8 +15,16 @@ const AddTask = ({ show, onClose, userId }) => {
         description,
         userId,
       })
-      .then(({ data }) => console.log(data))
+      .then(() => {
+        onSucceed();
+        onClose();
+        setTitle("");
+        setDescription("");
+        setValidated(false);
+        setLoading(false);
+      })
       .catch((e) => console.log(e));
+    setLoading(true);
   };
 
   return (
@@ -24,14 +33,18 @@ const AddTask = ({ show, onClose, userId }) => {
         <Modal.Title>Add new Task</Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        <Form>
+        <Form validated={validated}>
           <Form.Group controlId="addTask.Title">
             <Form.Label>Title</Form.Label>
             <Form.Control
               value={title}
               onChange={(e) => setTitle(e.target.value)}
               type="text"
+              required
             />
+            <Form.Control.Feedback type="invalid">
+              Please, provide a title
+            </Form.Control.Feedback>
           </Form.Group>
           <Form.Group controlId="addTask.Description">
             <Form.Label>Description</Form.Label>
@@ -40,7 +53,11 @@ const AddTask = ({ show, onClose, userId }) => {
               onChange={(e) => setDescription(e.target.value)}
               as="textarea"
               rows={3}
+              required
             />
+            <Form.Control.Feedback type="invalid">
+              Please, provide a description
+            </Form.Control.Feedback>
           </Form.Group>
         </Form>
       </Modal.Body>
@@ -48,9 +65,29 @@ const AddTask = ({ show, onClose, userId }) => {
         <Button variant="secondary" onClick={onClose}>
           Close
         </Button>
-        <Button variant="primary" onClick={saveTask}>
-          Save Changes
-        </Button>
+        {loading ? (
+          <Button variant="primary" disabled={true}>
+            <Spinner
+              as="span"
+              animation="border"
+              size="sm"
+              role="status"
+              aria-hidden="true"
+            />
+          </Button>
+        ) : (
+          <Button
+            variant="primary"
+            onClick={() => {
+              setValidated(true);
+              if (title && description) {
+                saveTask();
+              }
+            }}
+          >
+            Save Changes
+          </Button>
+        )}
       </Modal.Footer>
     </Modal>
   );
