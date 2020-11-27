b620badb08cfc1f5d259760130c08e1fbf60d677
Xalsar <xavicarreragimbert@gmail.com>
2020-11-27 22:35:28 +0100

    Save user AddTask

--- frontend/src/components/AddTask/AddTask.js
+++ frontend/src/components/AddTask/AddTask.js
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import { Modal, Button, Form } from "react-bootstrap";
 
-const AddTask = ({ show, onClose }) => {
+const AddTask = ({ show, onClose, userId }) => {
+  console.log(userId);
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
+  const saveTask = () => {
+    axios
+      .post("/api/users/", {
+        title,
+        description,
+        userId,
+      })
+      .then(({ data }) => console.log(data))
+      .catch((e) => console.log(e));
+  };
+
   return (
     <Modal show={show} onHide={onClose} size="lg">
       <Modal.Header closeButton>
@@ -11,11 +27,20 @@ const AddTask = ({ show, onClose }) => {
         <Form>
           <Form.Group controlId="addTask.Title">
             <Form.Label>Title</Form.Label>
-            <Form.Control type="text" />
+            <Form.Control
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+              type="text"
+            />
           </Form.Group>
           <Form.Group controlId="addTask.Description">
             <Form.Label>Description</Form.Label>
-            <Form.Control as="textarea" rows={3} />
+            <Form.Control
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+              as="textarea"
+              rows={3}
+            />
           </Form.Group>
         </Form>
       </Modal.Body>
@@ -23,7 +48,7 @@ const AddTask = ({ show, onClose }) => {
         <Button variant="secondary" onClick={onClose}>
           Close
         </Button>
-        <Button variant="primary" onClick={onClose}>
+        <Button variant="primary" onClick={saveTask}>
           Save Changes
         </Button>
       </Modal.Footer>
